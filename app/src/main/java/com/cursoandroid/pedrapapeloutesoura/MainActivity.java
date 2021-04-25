09498aa6e5d589bff0e6dd25cf1c1034d50d3338
Edigar Maia <edigarmaia@gmail.com>
2021-04-25 02:06:01 -0300

    Projeto finalizado

--- app/src/main/java/com/cursoandroid/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/cursoandroid/pedrapapeloutesoura/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -30,26 +31,44 @@ public class MainActivity extends AppCompatActivity {
         this.opcaoSelecionada("tesoura");
     }
 
-    public void opcaoSelecionada(String opcaoSelecionada){
+    public void opcaoSelecionada(String escolhaUsuario) {
         ImageView imagemResultado = findViewById(R.id.imageResultado);
+        TextView textResultado = findViewById(R.id.textResultado);
 
         int numero = new Random().nextInt(3);//0, 1 e 2
 
         String[] opcoes = {"pedra", "papel", "tesoura"};
-        String opcaoApp = opcoes[ numero ];
+        String escolhaApp = opcoes[numero];
 
-        switch (opcaoApp){
+        switch (escolhaApp) {
             case "pedra":
-                imagemResultado.setImageResource(R.drawable.pedra);
+                imagemResultado.setImageResource(R.drawable.pedra);//carrega a imagem selcecionada
                 break;
             case "papel":
                 imagemResultado.setImageResource(R.drawable.papel);
             case "tesoura":
                 imagemResultado.setImageResource(R.drawable.tesoura);
-                
 
         }
-        System.out.println("Ítem clicado: " + opcaoApp);
+        if (
+                (escolhaApp == "tesoura" && escolhaUsuario == "papel") ||
+                (escolhaApp == "papel" && escolhaUsuario == "pedra") ||
+                (escolhaApp == "pedra" && escolhaUsuario == "tesoura")
+
+        ){//App ganhador
+
+           textResultado.setText("Você perdeu!");
+        } else if( //Usuario ganhador
+            (escolhaUsuario == "tesoura" && escolhaApp == "papel") ||
+            (escolhaUsuario == "papel" && escolhaApp == "pedra") ||
+            (escolhaUsuario == "pedra" && escolhaApp == "tesoura"))
+        {
+            textResultado.setText("Você ganhou!");
+
+        } else {//Empate
+           textResultado.setText("Empatamos!");
+        }
+        System.out.println("Ítem clicado: " + escolhaApp);
 
     }
 
